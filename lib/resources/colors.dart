615ca72177c7ad99e417127ef978e7c70544e930
Drakx116 <alexandre2.guyot@gmail.com>
2022-05-10 22:50:57 +0200

    feat(assets): Add app colors constants :sparkles:

---
+++ lib/resources/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor
+{
+  static const Color PRIMARY = Color.fromRGBO(84, 13, 110, 1);
+  static const Color SECONDARY = Color.fromRGBO(238, 66, 102, 1);
+  static const Color TERTIARY = Color.fromRGBO(255, 210, 83, 1);
+  static const Color LIGHT = Color.fromRGBO(243, 252, 240, 1);
+  static const Color DARK = Color.fromRGBO(31, 39, 27, 1);
+}
