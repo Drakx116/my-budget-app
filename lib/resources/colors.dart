3f55b9516cc75b5075e7544461d8379aef88dd46
Drakx116 <alexandre2.guyot@gmail.com>
2022-05-14 13:58:37 +0200

    feat(style): adjust app colors :sparkles:

--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppColor
 {
-  static const Color PRIMARY = Color.fromRGBO(84, 13, 110, 1);
+  static const Color PRIMARY = Color.fromRGBO(93, 46, 140, 1);
   static const Color SECONDARY = Color.fromRGBO(238, 66, 102, 1);
   static const Color TERTIARY = Color.fromRGBO(255, 210, 83, 1);
+  static const Color QUATERNARY = Color.fromRGBO(95, 173, 65, 1);
+  static const Color QUINARY = Color.fromRGBO(0, 167, 225, 1);
   static const Color LIGHT = Color.fromRGBO(243, 252, 240, 1);
   static const Color DARK = Color.fromRGBO(31, 39, 27, 1);
 }
