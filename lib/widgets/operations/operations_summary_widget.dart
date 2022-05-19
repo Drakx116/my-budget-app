6222e4c5cdf1ac45a7d4203561007ed526a6fba7
Drakx116 <alexandre2.guyot@gmail.com>
2022-05-19 13:43:06 +0200

    fix(operation): prevent missing summary data issue :bug:

--- lib/widgets/operations/operations_summary_widget.dart
+++ lib/widgets/operations/operations_summary_widget.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:my_budget_app/constants/enums/operation_type.dart';
 import 'package:my_budget_app/resources/colors.dart';
 import 'package:my_budget_app/screens/auth/login_screen.dart';
 import 'package:my_budget_app/services/api_service.dart';
@@ -34,9 +35,21 @@ class OperationSummaryWidget extends StatelessWidget
                 return Container();
               }
 
-              var summary = List.from(summaryResponse.data);
-              String expenses = (summary.isNotEmpty) ? summary[0]['total'].toString() : '0';
-              String incomes = (summary.isNotEmpty) ? summary[1]['total'].toString() : '0';
+              Map summary = { 'income': 0, 'expense': 0 };
+
+
+              List.from(summaryResponse.data).forEach((element) {
+                if ('expense' == element['type']) {
+                  summary['expense'] = element['total'];
+                }
+
+                if ('income' == element['type']) {
+                  summary['income'] = element['total'];
+                }
+              });
+
+              String expenses = summary['expense'].toString() + '€';
+              String incomes = summary['income'].toString() + '€';
 
               return Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8),
@@ -48,7 +61,7 @@ class OperationSummaryWidget extends StatelessWidget
                         padding: const EdgeInsets.only(right: 4),
                         child: SingleValueTile(
                           label: 'Total des rentrées',
-                          value: incomes.toString() + '€',
+                          value: incomes.toString(),
                           backgroundColor: AppColor.QUATERNARY,
                         ),
                       ),
@@ -58,7 +71,7 @@ class OperationSummaryWidget extends StatelessWidget
                         padding: const EdgeInsets.only(left: 4),
                         child: SingleValueTile(
                           label: 'Total des dépenses',
-                          value: expenses.toString() + '€',
+                          value: expenses.toString(),
                           backgroundColor: AppColor.SECONDARY,
                         ),
                       ),
